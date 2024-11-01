c3db21543f220946ea429a532f7fcbd76e8be0d9
timothyloh0523 <141809967+timothyloh0523@users.noreply.github.com>
2024-11-02 06:00:09 +0800

    Update pixel_control.v

--- MODS.xpr/MODS/MODS.srcs/sources_1/new/pixel_control.v
+++ MODS.xpr/MODS/MODS.srcs/sources_1/new/pixel_control.v
@@ -27,16 +27,24 @@ module pixel_control(
     input [6:0] xref_std, [5:0] yref_std, 
     input [6:0] xref_e1, [5:0] yref_e1, 
     input [6:0] xref_e2, [5:0] yref_e2,
+    input [2:0] stnum,
     output [15:0] pixel_data
     );
-    
-    s1 draw_s1(
-        .xref(xref_std), .x(x), .yref(yref_std), .y(y), 
-        .clock(clock), .btnU(btnU), .btnL(btnL), .btnR(btnR), .oled_data(pixel_data)
+    wire [15:0] s1_oled;
+    student_control studcont(
+        .x(x), .y(y), .clock(clock), .btnU(btnU), .btnL(btnL), .btnR(btnR),
+        .xref_std(xref_std), .yref_std(yref_std), 
+        .xref_e1(xref_e1), .yref_e1(yref_e1), 
+        .xref_e2(xref_e2), .yref_e2(yref_e2),
+        .stnum(stnum),
+        .pixel_data(pixel_data)
         );
-    // module for drawing student
-        // within module for drawing student, call s1/s2/s3/s4/s5 via multiplexer
-    // module for drawing enemies taking in 5 pairs of xref,yref
+    // add module for drawing enemies taking in 5 pairs of xref,yref
         // within module for drawing enemies, include indiv draw_enemy modules
+    // add module for drawing muffin
+    
+    // always block: if (x within xref_1 and xref_1+7, and y within yref_1 and yref_1 + 7)
+    //                  then (pixel_data = 1_data)
+    //               repeat as many times as necessary, each new statement higher priority than previous (ie overwriting if needed)
     
 endmodule
