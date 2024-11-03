6196756e3d57fa70993b0bbe4ebd7b0bf4509801
timothy <timothyloh0523@ymail.com>
2024-11-04 02:13:09 +0800

    yeehaw

---
+++ MODS.srcs/sim_1/new/sim.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.11.2024 15:45:05
+// Design Name: 
+// Module Name: sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sim(
+
+    );
+    
+endmodule
