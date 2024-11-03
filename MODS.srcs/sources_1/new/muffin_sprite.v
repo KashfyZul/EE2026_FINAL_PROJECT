d77e79426258fd90844bb678d8735c874f9ed86c
timothyloh0523 <141809967+timothyloh0523@users.noreply.github.com>
2024-11-04 02:22:43 +0800

    Update colour of outline in muffin_sprite.v

--- MODS.srcs/sources_1/new/muffin_sprite.v
+++ MODS.srcs/sources_1/new/muffin_sprite.v
@@ -29,7 +29,7 @@ module muffin_sprite (
     wire [15:0] white = 16'b1111111111111111;
     wire [15:0] darkred = 16'b1110000100001100;
     wire [15:0] black = 0;
-//    wire [15:0] yellow = 16'b1111111101100111;
+    wire [15:0] brown = 16'h9A20;
     wire [15:0] yellow = 16'b1111111110101011;
     wire [15:0] placeholder = 1;
     always @ (x, y) begin 
@@ -38,7 +38,7 @@ module muffin_sprite (
             if (((pixel_index >= 20) && (pixel_index <= 22)) || pixel_index == 25 || ((pixel_index >= 28) && (pixel_index <= 30)) || pixel_index == 33 || ((pixel_index >= 36) && (pixel_index <= 38)) || (pixel_index >= 42) && (pixel_index <= 43)) oled_data = white;
             else if (pixel_index == 26 || pixel_index == 27 || pixel_index == 34 || pixel_index == 35) oled_data = darkred;
             else if (pixel_index == 41 || pixel_index == 44) oled_data = yellow;
-            else if ((pixel_index >= 11 && pixel_index <= 19) || pixel_index == 23 || pixel_index == 24 || pixel_index == 31 || pixel_index == 32 || pixel_index == 39 || pixel_index == 40 || (pixel_index >= 45 && pixel_index <=52)) oled_data = black;
+            else if ((pixel_index >= 11 && pixel_index <= 19) || pixel_index == 23 || pixel_index == 24 || pixel_index == 31 || pixel_index == 32 || pixel_index == 39 || pixel_index == 40 || (pixel_index >= 45 && pixel_index <=52)) oled_data = brown;
             else oled_data = placeholder;
         end
     end          
